d65e2cd5da04456a997e0b85059338b1d9bd8f81
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2015-05-20 21:57:28 +0200

    Set -fno-strict-aliasing here so we can get rid of it globally
    Add -fno-strict-aliasing to the compiler flags where it is actually
    needed so we can get rid of the flag globally -- without it, compilers
    can optimize better.
    
    Change-Id: Ie477fac9909b2082b52f187a05518c9cd28811a4
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:=clatd.c dump.c checksum.c translate.c icmp.c ipv4.c ipv6.c config.c dns64.c logging.c getaddr.c netlink_callbacks.c netlink_msg.c setif.c mtu.c tun.c ring.c
 
 LOCAL_CFLAGS := -Wall -Werror -Wunused-parameter
+LOCAL_CFLAGS += -fno-strict-aliasing
 LOCAL_C_INCLUDES := external/libnl/include bionic/libc/dns/include
 LOCAL_STATIC_LIBRARIES := libnl
 LOCAL_SHARED_LIBRARIES := libcutils liblog libnetutils
@@ -30,6 +31,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := clatd_test
 LOCAL_CFLAGS := -Wall -Werror -Wunused-parameter
+LOCAL_CFLAGS += -fno-strict-aliasing
 LOCAL_SRC_FILES := clatd_test.cpp checksum.c translate.c icmp.c ipv4.c ipv6.c logging.c config.c tun.c
 LOCAL_MODULE_TAGS := eng tests
 LOCAL_SHARED_LIBRARIES := liblog
@@ -43,5 +45,6 @@ LOCAL_MODULE := clatd_microbenchmark
 LOCAL_CFLAGS := -Wall -Werror -Wunused-parameter
 LOCAL_SRC_FILES := clatd_microbenchmark.c checksum.c tun.c
 LOCAL_MODULE_TAGS := eng tests
+LOCAL_CFLAGS += -fno-strict-aliasing
 
 include $(BUILD_NATIVE_TEST)
